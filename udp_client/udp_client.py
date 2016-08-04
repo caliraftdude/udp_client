5658061d2eff881d328b45460f07170276235598
Dan Holland <d.holland@f5.com>
2016-08-03 21:29:58 -0500

    Initial commit

---
+++ udp_client/udp_client.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+# Create socket obj
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send some data
+client.sendto("AAABBBCCC", (target_host, target_port))
+
+# Receive some data
+data, addr = client.recvfrom(4096)
+
+print addr
+print data
